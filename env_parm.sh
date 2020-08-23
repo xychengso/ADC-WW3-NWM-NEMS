0644106e159383a8642870cb171d500187065676
uri-xuanyu.chen <xychen@my.uri.edu>
2020-08-22 21:34:02 -0400

    XYC built on hatteras-renci

---
+++ env_parm.sh
@@ -0,0 +1,10 @@
+#!/bash/sh
+export LIBDIR=/projects/ees/dhs-crc/xychen
+export METIS_PATH=/home/xychen/localmodules/parmetis
+export ROOTDIR=/home/xychen/ADC-WW3-NWM-NEMS
+#export ESMFMKFILE=/projects/ees/dhs-crc/xychen/ESMFlib/lib/libg/Linux.intel.64.mvapich2.default/esmf.mk
+#export ESMFMKFILE=/projects/ees/dhs-crc/xychen/esmf-ESMF_8_0_0/install/lib/libg/Linux.intel.64.mvapich2.default/esmf.mk
+export ESMFMKFILE=/projects/ees/dhs-crc/xychen/ESMFlib_openmpi/lib/libg/Linux.intel.64.openmpi.default/esmf.mk
+#export ESMFMKFILE=/home/mjisan/regesm/reg/new/esmf-5.2.0r/install_dir/lib/libO/Linux.intel.64.intelmpi.default
+export SZIPHOME=/home/xychen/localmodules/szip/2.1/lib
+
